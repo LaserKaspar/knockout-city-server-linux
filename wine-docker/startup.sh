9d69de4742445b955f91851c0fb300e03b18b614
Felix Kaspar <ich@felixkaspar.com>
2023-06-09 15:55:13 +0200

    Finally working in docker!

--- wine-docker/startup.sh
+++ wine-docker/startup.sh
@@ -3,4 +3,4 @@
 sleep 1s
 cd KnockoutCityServer
 wine --version
-wine KnockoutCityServer.exe -backend_db="postgres://postgres:postgres@192.168.68.3:5431" -backend_redis_db_host="192.168.68.3" -backend-redis_db_port="6381"
+wine KnockoutCityServer.exe -backend_db="postgres://postgres:postgres@192.168.68.3:5431" -backend_redis_db_host="192.168.68.3" -backend-redis_db_port="6380"
